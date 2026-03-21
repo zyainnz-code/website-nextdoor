c6427ac8b6a553dba6a27f7cb1498905f8aad638
zyainnz-code <zyainnz@gmail.com>
2026-03-21 20:13:49 +0700

    form pendaftaran

--- index.html/script.js
+++ index.html/script.js
@@ -24,3 +24,24 @@ function openModal(src) {
 function closeModal() {
   document.getElementById("modalgw").style.display = "none";
 }
+
+const form = document.getElementById("registrationForm");
+if (form) {
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const name = form.querySelectorAll("input")[0].value;
+    const program = form.querySelector("select").value;
+
+    const waNumber = "6285250878278";
+    const text = `Halo Admin TOGA! Nama saya ${name}. Saya ingin mendaftar program ${program}. Mohon info selanjutnya ya!`;
+
+    // Saat tombol kirim diklik
+    document.getElementById("formContent").style.display = "none";
+    document.getElementById("successMessage").style.display = "block";
+
+    const waURL = `https://api.whatsapp.com/send?phone=${waNumber}&text=${text}`;
+    setTimeout(() => {
+      window.open(waURL, "_blank");
+    }, 1000);
+  });
+}
